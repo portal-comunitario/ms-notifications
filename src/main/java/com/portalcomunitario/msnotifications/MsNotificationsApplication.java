04548d7f6edd7d459a705758feb62ed6bab6bbd5
Juan Cáceres Salazar <juan.caceressalazar@duocuc.cl>
2026-06-29 21:43:26 +0000

    feat: initial structure — ms-notifications

---
+++ src/main/java/com/portalcomunitario/msnotifications/MsNotificationsApplication.java
@@ -0,0 +1,12 @@
+package com.portalcomunitario.msnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotificationsApplication.class, args);
+	}
+}
